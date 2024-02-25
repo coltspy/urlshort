cad1e035cfc51df49f92e75da539f25dddc326b4
Colton Spyker <93845598+coltspy@users.noreply.github.com>
2024-02-25 05:13:54 -0800

    Update main.go

--- main.go
+++ main.go
@@ -8,11 +8,24 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	type ExpirationOption struct {
+		Name     string
+		Duration time.Duration
+	}
+
+	var expirationOptions = []ExpirationOption{
+		{"1 Day", 24 * time.Hour},
+		{"1 Month", 30 * 24 * time.Hour},
+		{"1 Year", 365 * 24 * time.Hour},
+		{"Lifetime", 0},
+	}
+
 	// Connect to SQLite Database
 	db, err := sql.Open("sqlite3", "urlshortener.db")
 	if err != nil {
@@ -28,7 +41,8 @@ func main() {
         original_url TEXT,
         access_count INTEGER DEFAULT 0,
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        last_accessed_at DATETIME
+        last_accessed_at DATETIME,
+		expires_at DATETIME
     );`
 	_, err = db.Exec(createTableSQL)
 	if err != nil {
@@ -45,10 +59,11 @@ func main() {
 			r.ParseForm()
 			originalURL := r.FormValue("url")
 			customAlias := r.FormValue("customAlias")
+			expirationOption := r.FormValue("expiration")
+
 			var shortURL string
 
 			if customAlias != "" {
-				// Check if customAlias is provided and unique
 				var exists int
 				err := db.QueryRow("SELECT COUNT(*) FROM urls WHERE custom_alias = ?", customAlias).Scan(&exists)
 				if err != nil {
@@ -59,10 +74,10 @@ func main() {
 				if exists > 0 {
 					http.Error(w, "Custom alias already in use", http.StatusBadRequest)
 					return
+				} else {
+					shortURL = customAlias
 				}
-				shortURL = customAlias
 			} else {
-				// Generate a unique random short URL
 				for {
 					shortURL, err = generateRandomString(8) // Generate an 8 character string
 					if err != nil {
@@ -83,31 +98,51 @@ func main() {
 					}
 				}
 			}
+			var expirationDuration time.Duration
+			for _, option := range expirationOptions {
+				if option.Name == expirationOption {
+					expirationDuration = option.Duration
+					break
+				}
+			}
+
+			expiresAt := time.Now().Add(expirationDuration)
 
 			// Insert the URL and short URL (and custom alias if provided) into the database
-			_, err = db.Exec("INSERT INTO urls (original_url, short_url, custom_alias) VALUES (?, ?, ?)", originalURL, shortURL, customAlias)
+			_, err = db.Exec("INSERT INTO urls (original_url, short_url, custom_alias, expires_at) VALUES (?, ?, ?, ?)", originalURL, shortURL, customAlias, expiresAt) // Note the addition of expiresAt
 			if err != nil {
 				log.Printf("Failed to insert URL: %v", err)
 				http.Error(w, "Database error", http.StatusInternalServerError)
 				return
 			}
 
-			fmt.Fprintf(w, "URL shortened successfully: %s", shortURL)
+			fullURL := "https://2o.wtf/s/" + shortURL
+			fmt.Fprintf(w, "Shortened URL: <a href=\"%s\" target=\"_blank\">%s</a>", fullURL, fullURL)
+
 		}
 	})
 
 	// Handler to redirect to the original URL
 	http.HandleFunc("/s/", func(w http.ResponseWriter, r *http.Request) {
 		path := strings.TrimPrefix(r.URL.Path, "/s/")
-		var originalURL string
+		log.Printf("Requested path: %s", path) // Debugging log
 
-		err := db.QueryRow("SELECT original_url FROM urls WHERE short_url = ? OR custom_alias = ?", path, path).Scan(&originalURL)
+		var originalURL string
+		var expiresAt sql.NullTime // Use sql.NullTime to handle possible NULL values
+
+		err := db.QueryRow("SELECT original_url, expires_at FROM urls WHERE short_url = ? OR custom_alias = ?", path, path).Scan(&originalURL, &expiresAt)
 		if err != nil {
+			log.Printf("Database query error: %v", err) // Debugging log
 			if err == sql.ErrNoRows {
 				http.NotFound(w, r)
 			} else {
 				http.Error(w, "Internal server error", http.StatusInternalServerError)
 			}
+			return
+		}
+
+		if expiresAt.Valid && time.Now().After(expiresAt.Time) {
+			http.Error(w, "This URL has expired", http.StatusGone)
 			return
 		}
 
